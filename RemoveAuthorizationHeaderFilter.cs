be07fd53060240b56a369446b9e706622a6116e2
chaviyankelevick <chavi95445@gmail.com>
2023-04-24 14:13:54 +0300

    with nothings

--- RemoveAuthorizationHeaderFilter.cs
+++ RemoveAuthorizationHeaderFilter.cs
@@ -1,17 +1,17 @@
-using System.Linq;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
+// using System.Linq;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Mvc.Authorization;
+// using Microsoft.OpenApi.Models;
+// using Swashbuckle.AspNetCore.SwaggerGen;
 
-public class RemoveAuthorizationHeaderParameterFilter : IOperationFilter
-{
-    public void Apply(OpenApiOperation operation, OperationFilterContext context)
-    {
-        var authHeaderParameter = operation.Parameters.SingleOrDefault(p => p.Name == "Authorization");
-        if (authHeaderParameter != null)
-        {
-            operation.Parameters.Remove(authHeaderParameter);
-        }
-    }
-}
+// public class RemoveAuthorizationHeaderParameterFilter : IOperationFilter
+// {
+//     public void Apply(OpenApiOperation operation, OperationFilterContext context)
+//     {
+//         var authHeaderParameter = operation.Parameters.SingleOrDefault(p => p.Name == "Authorization");
+//         if (authHeaderParameter != null)
+//         {
+//             operation.Parameters.Remove(authHeaderParameter);
+//         }
+//     }
+// }
