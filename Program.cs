ca9528b423d702bfbfdce3116c80a8bf6effce8c
chaviyankelevick <chavi95445@gmail.com>
2023-04-24 14:02:53 +0300

    without tokens

--- Program.cs
+++ Program.cs
@@ -34,29 +34,29 @@ builder.Services.AddCors(options =>
         .AllowAnyHeader());});
 builder.Services.AddSwaggerGen(options =>
 {
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Description = "Bearer Authentication with JWT Token",
-        Type = SecuritySchemeType.Http
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-        Reference = new OpenApiReference
-                {
-                    Id = "Bearer",
-                    Type = ReferenceType.SecurityScheme
-                }
-            },
-            new List<string>()
-        }
-    });
+    // options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    // {
+    //     Scheme = "Bearer",
+    //     BearerFormat = "JWT",
+    //     In = ParameterLocation.Header,
+    //     Name = "Authorization",
+    //     Description = "Bearer Authentication with JWT Token",
+    //     Type = SecuritySchemeType.Http
+    // });
+    // options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    // {
+    //     {
+    //         new OpenApiSecurityScheme
+    //         {
+    //     Reference = new OpenApiReference
+    //             {
+    //                 Id = "Bearer",
+    //                 Type = ReferenceType.SecurityScheme
+    //             }
+    //         },
+    //         new List<string>()
+    //     }
+    // });
 }
 );
 
